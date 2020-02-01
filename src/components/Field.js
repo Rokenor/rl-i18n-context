a5e8c394e5d582159256b4bbfab5db476ae3b1da
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-02-02 01:46:41 +0200

    context like redux

--- src/components/Field.js
+++ src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === 'english' ? 'Name': 'Iм\'я';
+    const text = this.context.language === 'english' ? 'Name': 'Iм\'я';
     return (
       <div className="ui field">
         <label>{text}</label>
